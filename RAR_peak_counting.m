43ab70e5b4c19e7bebacd3f78b88ce06f146bbef
Robert Rifkin <rr3302@cumc.columbia.edu>
2021-07-29 14:04:46 -0400

    updated peak counting algorithm, able to have different start and stop times

--- RAR_peak_counting.m
+++ RAR_peak_counting.m
@@ -1,9 +1,21 @@
-function RAR_peak_counting (calc, times)
+function RAR_peak_counting (calc, times, first_start, first_end, second_start, second_end)
+
+	% This function takes as input normalized calcium amplitudes ('calc') and time in seconds ('times'), 
+	% counts the number of spikes above a threshold per channel, and calculates the mean and 95% CI.
+	% This is performed for two different epochs within the recording (e.g. drug vs. non-drug)
+	% The imaging data are recorded at 50 Hz, so there are 3000 pts per minute and 45000 per 15 minute epoch.
+	% The start and end times for each epoch (e.g. 'first_start') are specified in data points (50 per second).
+
+	% default values
+	% first_start = 1;
+	% first_end = 45000;
+	% second_start = 45001;
+	% second_end = 90000;
 
     % iterates through channels and counts peaks at least 1% higher than baseline during zmg only
 	for ch = 1:96
 		
-    	[pks, locs] = findpeaks(calc(ch,1:45000),times(1:45000),'MinPeakHeight',1.01,'MinPeakDistance',1);
+    	[pks, locs] = findpeaks(calc(ch,first_start:first_end),times(first_start:first_end),'MinPeakHeight',1.01,'MinPeakDistance',1);
         pks_per_ch_early(ch) = length(locs);
     end
 
@@ -17,7 +29,7 @@ function RAR_peak_counting (calc, times)
 
 	% iterates through channels and counts peaks at least 1% higher than baseline during giga1 only
     for ch = 1:96
-        [pks, locs] = findpeaks(calc(ch,45001:90000),times(45001:90000),'MinPeakHeight',1.01,'MinPeakDistance',1);
+        [pks, locs] = findpeaks(calc(ch,second_start:second_end),times(second_start:second_end),'MinPeakHeight',1.01,'MinPeakDistance',1);
         pks_per_ch_late(ch) = length(locs);
         pk_amps_late{ch} = pks;
     end
