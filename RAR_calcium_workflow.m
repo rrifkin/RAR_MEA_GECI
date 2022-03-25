5fba1c5df55903b9751ec066f1fd7ecc634c1554
Robert Rifkin <rr3302@cumc.columbia.edu>
2022-03-25 12:21:04 -0400

    trying to make this into a single workflow

--- RAR_calcium_workflow.m
+++ RAR_calcium_workflow.m
@@ -1,7 +1,18 @@
 % Main script for processing a calcium imaging experiment
 
-% define constants
-radius = '';
-Eleclocs_file = '';
-ACSF_image_directory = '';
-ZMG_image_directory = '';
+function RAR_calcium_workflow (radius, eleclocs_file, ACSF_image_directory, ZMG_image_directory)
+
+	% define constants
+	radius = '';
+	eleclocs_file = '';
+	ACSF_image_directory = '';
+	ZMG_image_directory = '';
+
+	% Process raw calcium data
+	RAR_caproc_parloop (radius, eleclocs_file, number_of_tifs, tif_filename_prefix, tif_filename_suffix, last_tif_frames, output_filename)
+
+	% Pearson correlation of of Calcium ROIs
+	RAR_normalization_ROI_only (metadata_file, raw_acsf, raw_zmg)
+	RAR_Pearson_correlation_wilcoxon (data)
+
+end
--- RAR_convert_tif_to_video.m
+++
@@ -1,7 +0,0 @@
-function RAR_convert_tif_to_video (filename)
-
-	[X,map] = imread (filename);
-	imshow(X,map);
-	imcontrast
-
-end
