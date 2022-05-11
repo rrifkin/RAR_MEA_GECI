3f1a7f1b9f265e68b6ed002eb13e9e70b2f858a9
Robert Rifkin <rr3302@cumc.columbia.edu>
2022-05-11 11:13:10 -0400

    working on workflow

--- RAR_calcium_workflow/RAR_caproc_parloop.m
+++ RAR_calcium_workflow/RAR_caproc_parloop.m
@@ -1,9 +1,6 @@
 function RAR_caproc_parloop (radius, eleclocs_file, number_of_tifs, tif_filename_prefix, tif_filename_suffix, last_tif_frames, output_filename)
 
-	disp ('here');
-	disp (eleclocs_file);
-	load(eleclocs_file);
-
+	load(eleclocs_file,'-mat','Eleclocs');
 
 	pi_int = cell(1, number_of_tifs);
 	nFrames = 2040 * ones(1, number_of_tifs);
@@ -14,7 +11,8 @@ function RAR_caproc_parloop (radius, eleclocs_file, number_of_tifs, tif_filename
 		disp (num2str(i));
 		disp([9 'Working on tiff ' num2str(i) ' of ' num2str(number_of_tifs)]);
 		disp(Eleclocs);
-		pi_int{i+1} = chan_intensity_saturn([tif_filename_prefix num2str(i) tif_filename_suffix],radius,nFrames(i+1),Eleclocs);
+		filename = strcat(tif_filename_prefix, '_', num2str(i), tif_filename_suffix);
+		pi_int{i+1} = chan_intensity(filename,radius,nFrames(i+1),Eleclocs);
 	end
 	output = cell2mat(pi_int); % this may need to be transposed, (i.e. cell2mat(pi_int');) need to test with the actual data first
 
