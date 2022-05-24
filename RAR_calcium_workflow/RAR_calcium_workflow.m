378e4967d1692c4fa177957810e340c3d5d3337d
Robert Rifkin <rr3302@cumc.columbia.edu>
2022-05-24 07:43:19 -0400

    debugging workflow, works now

--- RAR_calcium_workflow/RAR_calcium_workflow.m
+++ RAR_calcium_workflow/RAR_calcium_workflow.m
@@ -1,27 +1,30 @@
 % Main script for processing a calcium imaging experiment
 
-function RAR_calcium_workflow (radius, eleclocs_file, ACSF_image_directory, ZMG_image_directory)
+function RAR_calcium_workflow (radius, eleclocs_file, ACSF_image_directory, ZMG_image_directory, metadata_file)
 
 	% number of frames in last tif file for each experiment phase
 	ACSF_last_tif_frames = 720 ;
 	ZMG_last_tif_frames = 240 ;
+
+	% current path
+	current_path = '~/Desktop';
 
 	% Process ACSF raw calcium data
 	cd (ACSF_image_directory) ; % navigate to ACSF image directory
 	ACSF_list_of_tifs = dir ("*.tif") ; % get all the filenames 
 	ACSF_number_of_tifs =  length (ACSF_list_of_tifs) ; % count number of tifs in the directory
 	ACSF_filename_prefix = erase(ACSF_list_of_tifs(1,1).name,".ome.tif"); % extract filename prefix from first element of list of tifs
-	ACSF_output_filename = strcat(ACSF_filename_prefix, ".mat"); 
+	ACSF_output_filename = strcat(current_path, "/", ACSF_filename_prefix, ".mat"); 
+	disp(ACSF_output_filename)
 	RAR_caproc_parloop (radius, eleclocs_file, ACSF_number_of_tifs, ACSF_filename_prefix, ".ome.tif", ACSF_last_tif_frames, ACSF_output_filename) ; 
 
-	
 	% Process ZMG raw calcium data
 	cd (ZMG_image_directory) ; % navigate to ZMG image directory
 	ZMG_list_of_tifs = dir ("*.tif") ; % get all the filenames 
 	ZMG_number_of_tifs =  length (ZMG_list_of_tifs) ; % count number of tifs in the directory
-	ZMG_filename_prefix = erase(ZMG_list_of_tifs(1,1).name,".tif"); % extract filename prefix from first element of list of tifs
-	ZMG_output_filename = strcat(ZMG_filename_prefix, ".mat"); 
-	RAR_caproc_parloop (radius, eleclocs_file, ZMG_number_of_tifs, ZMG_filename_prefix, ".tif", ZMG_last_tif_frames, ZMG_output_filename) ; 
+	ZMG_filename_prefix = erase(ZMG_list_of_tifs(1,1).name,".ome.tif"); % extract filename prefix from first element of list of tifs
+	ZMG_output_filename = strcat(current_path, "/", ZMG_filename_prefix, ".mat"); 
+	RAR_caproc_parloop (radius, eleclocs_file, ZMG_number_of_tifs, ZMG_filename_prefix, ".ome.tif", ZMG_last_tif_frames, ZMG_output_filename) ; 
 
 	% Pearson correlation of of Calcium ROIs
 	RAR_normalization_ROI_only (metadata_file, ACSF_output_filename, ZMG_output_filename);
--- RAR_calcium_workflow/RAR_caproc_parloop.m
+++ RAR_calcium_workflow/RAR_caproc_parloop.m
@@ -5,16 +5,22 @@ function RAR_caproc_parloop (radius, eleclocs_file, number_of_tifs, tif_filename
 	pi_int = cell(1, number_of_tifs);
 	nFrames = 2040 * ones(1, number_of_tifs);
 	nFrames(number_of_tifs) = last_tif_frames;
+
+	% Run chan_intensity on the first file (which does not have a number)
 	filename = strcat(tif_filename_prefix, tif_filename_suffix);
+	disp (strcat('Working on file ', {' '}, filename));
 	pi_int{1} = chan_intensity(filename,radius,2040,Eleclocs);
+
+	% Run chan_intensity on the remaining files (which have a number)
 	parfor i = 1:(number_of_tifs - 1)
-		disp([9 'Working on tiff ' num2str(i) ' of ' num2str(number_of_tifs)]);
 		filename = strcat(tif_filename_prefix, '_', num2str(i), tif_filename_suffix);
+		disp (strcat('Working on file ', {' '}, filename));
 		pi_int{i+1} = chan_intensity(filename,radius,nFrames(i+1),Eleclocs);
 	end
 	output = cell2mat(pi_int); % this may need to be transposed, (i.e. cell2mat(pi_int');) need to test with the actual data first
 
 	% save workspace to a file
-	save (output_filename,output);
+	disp (strcat('Saving result to', {' '}, output_filename));
+	save (output_filename);
 
 end
