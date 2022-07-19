40797edb4e3223cbc7da7302af49900edbbc3fbf
Robert Rifkin <rr3302@cumc.columbia.edu>
2022-07-19 11:36:32 -0400

    tidied up plotting function, more OOP

--- RAR_plot_traces.m
+++ RAR_plot_traces.m
@@ -4,38 +4,43 @@ function RAR_plot_traces(samples, amplitudes, sample_rate, channels, output_file
 
 	% parameters
 	vertical_increment = 200;
-	y_minimum = -200;
+
+	% instantiate the figure and axes with names
+	fig = figure;
+	ax = axes(fig);
 
 	% plot sequential traces in ascending order
-	hold on;
+	hold (ax, 'on');
 	offset = 0 ; % is incremented to make channels appear above each other
 	for index = 1:channels
 		y_offset = amplitudes(index,:) + offset ;
-		plot(samples, y_offset);
+		plot(ax, samples, y_offset);
 		offset = offset + vertical_increment ; 
     end
+	hold (ax, 'off');
 
-	fig = gcf;
-	ax = gca;
-
-	% determine parameters for axes
+	% calculate x-axis properties
 	tick_interval = (sample_rate * 60); % set a tick mark every 1 minute 
 	num_samples = length (samples);
 	minutes = num_samples / sample_rate / 60 ;
+
+	% calculate y-axis properties
+	y_minimum = -1 * vertical_increment;
 	y_maximum = (channels * vertical_increment); 
+	y_maximum_tick = y_maximum - vertical_increment;
 
-	% set parameters of axes
+	% set x-axis properties
 	ax.XLim =[0, num_samples];
-    ax.YLim = [y_minimum,y_maximum];
-
 	ax.XTick = 0:tick_interval:num_samples ;
 	ax.XTickLabel = 0:minutes;
 
-	ax.YTick = 0:vertical_increment:y_maximum;
+	% set y-axis properties
+	ax.YLim = [y_minimum,y_maximum];
+	ax.YTick = 0:vertical_increment:y_maximum_tick;
 	ax.YTickLabel = 1:channels;
 
+	% set axis and figure properties
 	ax.FontSize = 6;
+	fig.Position = [0, 0, 8000, 40000];
 
-    set(gcf, 'Position', [100, 100, 8000, 40000])
-	exportgraphics(gcf,output_file, 'ContentType', 'vector');
-    hold off;
+	exportgraphics(fig,output_file, 'ContentType', 'vector');
