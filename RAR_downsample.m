c4bc4137220190a8cca4bf73c6b89261ea8f1d56
snx27b <47576646+snx27b@users.noreply.github.com>
2022-08-03 14:43:11 -0400

    Basically working, able to plot LFP data

--- RAR_downsample.m
+++
@@ -1,6 +0,0 @@
-function [output_acsf, output_zmg] = RAR_downsample (slice_acsf, slice_zmg)
-
-	output_acsf = DownSamplingAndDetectMUA('seizure_recording', slice_acsf, 'baseline_recording', slice_acsf,'T', [0 600],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', 'output_acsf');
-	output_zmg = DownSamplingAndDetectMUA('seizure_recording', slice_zmg, 'baseline_recording', slice_acsf,'T', [0 1800],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', 'output_zmg');
-
-end
