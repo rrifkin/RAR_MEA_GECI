2ee2d3f5e488f315ee9bd9fff3022a15ce686582
snx27b <47576646+snx27b@users.noreply.github.com>
2022-08-09 13:52:43 -0400

    seems to be working

--- RAR_ephys_workflow.m
+++ RAR_ephys_workflow.m
@@ -1,17 +1,25 @@
 % Main script for processing a multielectrode recording experiment
 
-function RAR_ephys_workflow (ACSF_file, ZMG_file)
+function RAR_ephys_workflow (ACSF_file, ZMG_files)
 
-	[ ~ , ZMG_output_filename, ~ ] = fileparts(ZMG_file);
 	[ ~ , ACSF_output_filename, ~ ] = fileparts(ACSF_file);
-
+	ACSF_file = char (ACSF_file);
+	ACSF_output_filename = char(ACSF_output_filename);
 	[ACSF_MUA, ACSF_LFP] = DownSamplingAndDetectMUA('seizure_recording', ACSF_file, 'baseline_recording', ACSF_file,'T', [0 600],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', ACSF_output_filename);
-	[ZMG_MUA, ZMG_LFP] = DownSamplingAndDetectMUA('seizure_recording', ZMG_file, 'baseline_recording', ACSF_file,'T', [0 1800],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', ZMG_output_filename);
 
-	time = [1:3600000] ;  % creates an array of time in seconds corresponding to 1800 second ZMG file
+	LFP_data = [ACSF_LFP];
+	for current_ZMG_file = ZMG_files
+		[ ~ , current_ZMG_output_filename, ~ ] = fileparts(current_ZMG_file);
+		current_ZMG_file = char (current_ZMG_file);
+		current_ZMG_output_filename = char (current_ZMG_output_filename);
+		[current_ZMG_MUA, current_ZMG_LFP] = DownSamplingAndDetectMUA('seizure_recording', current_ZMG_file, 'baseline_recording', ACSF_file,'T', [0 1800],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', current_ZMG_output_filename);
+		LFP_data = [LFP_data, current_ZMG_LFP];
+	end
 
-	PDF_output_filename = strcat(ZMG_output_filename, '.pdf');
+	LFP_length = length(LFP_data);
 
-	RAR_plot_traces (time, ZMG_LFP, 2000, 96, PDF_output_filename);
+	% PDF_output_filename = strcat(ZMG_output_filename, '.pdf');
+
+	RAR_plot_traces (LFP_length, LFP_data, 2000, 96, "output.pdf");
 
 end
