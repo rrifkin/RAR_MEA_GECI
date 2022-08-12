ac927a1f408cdbae56bed3dcf240398fdedf8022
snx27b <47576646+snx27b@users.noreply.github.com>
2022-08-12 11:00:02 -0400

    plotting working; need to change ephys_workflow

--- RAR_ephys_workflow.m
+++ RAR_ephys_workflow.m
@@ -2,24 +2,36 @@
 
 function RAR_ephys_workflow (ACSF_file, ZMG_files)
 
-	[ ~ , ACSF_output_filename, ~ ] = fileparts(ACSF_file);
 	ACSF_file = char (ACSF_file);
-	ACSF_output_filename = char(ACSF_output_filename);
-	[ACSF_MUA, ACSF_LFP] = DownSamplingAndDetectMUA('seizure_recording', ACSF_file, 'baseline_recording', ACSF_file,'T', [0 600],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', ACSF_output_filename);
-
+	[ ~ , ACSF_filename, ~ ] = fileparts(ACSF_file);
+	[ACSF_MUA, ACSF_LFP] = DownSamplingAndDetectMUA('seizure_recording', ACSF_file, 'baseline_recording', ACSF_file,'T', [0 600],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', ACSF_filename);
+	current_length = length(ACSF_LFP)
+	split_products = split(ACSF_filename,",");
+	current_label = split_products(end)
+	labels = {};
+	current_label_array = {current_label{:}, current_length};
+	labels = {current_label_array}
 	LFP_data = [ACSF_LFP];
+
 	for current_ZMG_file = ZMG_files
-		[ ~ , current_ZMG_output_filename, ~ ] = fileparts(current_ZMG_file);
-		current_ZMG_file = char (current_ZMG_file);
-		current_ZMG_output_filename = char (current_ZMG_output_filename);
-		[current_ZMG_MUA, current_ZMG_LFP] = DownSamplingAndDetectMUA('seizure_recording', current_ZMG_file, 'baseline_recording', ACSF_file,'T', [0 1800],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', current_ZMG_output_filename);
-		LFP_data = [LFP_data, current_ZMG_LFP];
-	end
+		current_ZMG_file = char(current_ZMG_file);
+		[ ~ , current_ZMG_filename, ~ ] = fileparts(current_ZMG_file);
+		[current_ZMG_MUA, current_ZMG_LFP] = DownSamplingAndDetectMUA('seizure_recording', current_ZMG_file, 'baseline_recording', ACSF_file,'T', [0 1800],'T_baseline',[0 60], 'rms_estimation', 'median', 'savefile', current_ZMG_filename);
+		LFP_data = [LFP_data, current_ZMG_LFP]
+		current_length = length(current_ZMG_LFP)
+		split_products = split(current_ZMG_filename,",");
+		current_label = split_products(end)
+		current_label_array = {current_label{:}, current_length};
+		labels = {labels; current_label_array}
+    end
+
+    LFP_data = RAR_plot_LFP (filenames{:})
 
 	LFP_length = length(LFP_data);
+    LFP_samples = [1:LFP_length];
 
 	% PDF_output_filename = strcat(ZMG_output_filename, '.pdf');
 
-	RAR_plot_traces (LFP_length, LFP_data, 2000, 96, "output.pdf");
+	RAR_plot_traces (LFP_samples, LFP_data, 2000, 96, "output.pdf");
 
 end
--- RAR_plot_traces.m
+++ RAR_plot_traces.m
@@ -10,14 +10,15 @@ function RAR_plot_traces(samples, amplitudes, sample_rate, channels, output_file
 	ax = axes(fig);
 
 	% plot sequential traces in ascending order
-	hold (ax, 'on');
-	offset = 0 ; % is incremented to make channels appear above each other
+	hold (ax, 'on'); 
+    plot(samples,amplitudes(1,:))
+    offset = 0 ; % is incremented to make channels appear above each other
 	for index = 1:channels
 		y_offset = amplitudes(index,:) + offset ;
-		plot(ax, samples, y_offset);
-		offset = offset + vertical_increment ; 
+		plot(samples, y_offset);
+        offset = offset + vertical_increment ; 
     end
-	hold (ax, 'off');
+	hold (ax, 'off'); 
 
 	% calculate x-axis properties
 	tick_interval = (sample_rate * 60); % set a tick mark every 1 minute 
@@ -41,6 +42,6 @@ function RAR_plot_traces(samples, amplitudes, sample_rate, channels, output_file
 
 	% set axis and figure properties
 	ax.FontSize = 6;
-	fig.Position = [0, 0, 8000, 40000];
+	fig.Position = [0, 0, 8000, 100000];
 
 	exportgraphics(fig,output_file, 'ContentType', 'vector');
