c045acf46458a79ee4440e9d2bede46dcd7e2f10
snx27b <47576646+snx27b@users.noreply.github.com>
2022-08-23 14:39:51 -0400

    Update RAR_linelength.m
    linelength

--- RAR_linelength.m
+++ RAR_linelength.m
@@ -2,8 +2,8 @@ function RAR_linelength (LFP_file, artifact_file, bad_channels_file)
 
     % Import LFP data
     LFP_data = importdata (LFP_file);
-	artifact_samples = importdata(artifact_file)
-	bad_channels = importdata (bad_channels_file)
+	artifact_samples = importdata(artifact_file);
+	bad_channels = importdata (bad_channels_file);
 
     % Determine the length of the concatenated LFP data and make an array
     % representing each sample
@@ -21,14 +21,17 @@ function RAR_linelength (LFP_file, artifact_file, bad_channels_file)
     num_channels = length(LFP_data(:,1));
 
     % iterates through channels and determines line length
-	linelength = zeros(num_channels, 1); 
+	linelength = zeros(num_channels, 1);
 	for ch = 1:num_channels
 		initial_amplitude = LFP_data(ch,1);
-		for sample = 2:LFP_samples
+		for sample = LFP_samples(2:end)
     		current_amplitude = LFP_data(ch,sample);
-			diff = abs(current_amplitude - initial_amplitude);
-			linelength(ch) = linelength(ch) + diff; 
-			initial_amplitude = current_amplitude; 
+            if isnan(current_amplitude) == 0
+			    diff = abs(current_amplitude - initial_amplitude);
+			    linelength(ch) = linelength(ch) + diff;
+			    initial_amplitude = current_amplitude;
+            end
+        end
     end
 
     % calculates mean number of peaks per channel
