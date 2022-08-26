2be0bde5305e1110f7725eeda36d5d5a436b78cb
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2022-08-26 16:50:23 -0400

    more sophisticated exclusion criteria for folders

--- RAR_process_directory.m
+++ RAR_process_directory.m
@@ -7,7 +7,9 @@ function RAR_process_directory (input_path)
 	file_paths = {};
 	for i = 1:num_files
 		current_path = [file_list(i).folder, '/', file_list(i).name];
-		file_paths = [file_paths; current_path];
+		if ((contains (current_path, 'EXCLUDE') == 0) && (contains(current_path, 'WT Non-Tumor MEA GiGA1 vs. Vehicle') == 1))
+			file_paths = [file_paths; current_path];
+		end
 	end
 
 	disp (file_paths);
