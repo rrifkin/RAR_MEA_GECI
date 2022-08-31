1c61fb14a8585d33145c9313f377f925549a7634
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2022-08-31 13:34:32 -0400

    troubleshooting csv file reading

--- RAR_linelength.m
+++ RAR_linelength.m
@@ -2,9 +2,9 @@ function RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_ch
 
     % Import LFP data
     LFP_data = importdata (LFP_file);
-	artifact_samples = importdata(artifact_file);
-	bad_channels = importdata (bad_channels_file);
-    offslice_channels = importdata (offslice_channels_file);
+	artifact_samples = readmatrix(artifact_file);
+	bad_channels = readmatrix (bad_channels_file)
+    offslice_channels = readmatrix (offslice_channels_file)
 
     % Delete selected time ranges (columns) from LFP_data. 
     % artifact_samples is a N x 2 array where N is the number of
@@ -18,9 +18,9 @@ function RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_ch
     LFP_data(:,artifact_range) = [];
 
     % delete selected channels (rows) from LFP_data
-    excluded_channels = [bad_channels, offslice_channels]; 
-    excluded_channels = unique(excluded_channels);
-    LFP_data(excluded_channels(:),:) = [] ; 
+    excluded_channels = [bad_channels, offslice_channels]
+    excluded_channels = unique(excluded_channels)
+    LFP_data(excluded_channels(:),:) = [] ;
 
     % Calculates linelength
     linelength = sum(abs(diff(LFP_data,[],2)'))./size(LFP_data,2);
@@ -28,13 +28,13 @@ function RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_ch
 
     % Output data to .csv file
 	output_array = ["mean linelength per channel per sample", mean_linelength];
-	output_file = strcat(LFP_file, '_linelength_v2.csv');
+	output_file = strcat(LFP_file(1:end-16), '_linelength_v2.csv');
 	writematrix(output_array, output_file);
 
     % Plot actually analyzed data for sanity-checking
-    clean_plot_file = strcat(LFP_file, '_linelength_v2_analyzed_data.pdf');
-    LFP_samples = 1:length(LFP_data(1,:);
-    num_channels = length(LFP_data(:,1));
-    RAR_plot_traces (LFP_samples, LFP_data, 2000, num_channels, clean_plot_file);
+    %clean_plot_file = strcat(LFP_file, '_linelength_v2_analyzed_data.pdf');
+    %LFP_samples = 1:length(LFP_data(1,:));
+    %num_channels = length(LFP_data(:,1));
+    %RAR_plot_traces (LFP_samples, LFP_data, 2000, num_channels, clean_plot_file);
 
 end
--- RAR_workflows/RAR_workflow_linelength.m
+++ RAR_workflows/RAR_workflow_linelength.m
@@ -1,9 +1,30 @@
 
-function RAR_workflow_linelength (LFP_file)
+function RAR_workflow_linelength (input_path)
 
-	artifact_file = strcat(LFP_file(1:end-16), '_artifact_samples.csv');
-	bad_channels_file = strcat(LFP_file(1:end-16), '_bad_channels.csv');
-	offslice_channels_file = strcat(LFP_file(1:end-16), '_offslice_channels.csv');
-	RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_channels_file);
+	file_list = dir ([input_path, '/**/*_artifact_samples.csv']);
+	num_files = length (file_list);
+
+	file_paths = {};
+	for i = 1:num_files
+		current_path = [file_list(i).folder, '/', file_list(i).name];
+		if ((contains (current_path, 'EXCLUDE') == 0) && (contains(current_path, 'WT Non-Tumor MEA GiGA1 vs. Vehicle') == 1))
+			file_paths = [file_paths; current_path];
+		end
+	end
+
+	disp (file_paths);
+
+	for i = 1:length(file_paths)
+		disp(i);
+		artifact_file = file_paths{i};
+		bad_channels_file = strcat(artifact_file(1:end-21), '_bad_channels.csv');
+		offslice_channels_file = strcat(artifact_file(1:end-21), '_offslice_channels.csv');
+		LFP_file = strcat(artifact_file(1:end-21), '_NSxFile_LFP.mat')
+		RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_channels_file);
+
+	end
+
+
+	
 
 end
