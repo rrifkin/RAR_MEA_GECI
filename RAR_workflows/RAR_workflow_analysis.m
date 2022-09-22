23bcb2bfad2367d7e73bfb4a403cc0cac6b20ac8
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2022-09-22 15:22:26 -0400

    updated RAR_workflow_analysis

--- RAR_workflows/RAR_workflow_analysis.m
+++ RAR_workflows/RAR_workflow_analysis.m
@@ -1,19 +1,30 @@
+% Does LFP analyses on all files (that have had the appropriate manual steps
+% performed) in a given directory.
 
 function RAR_workflow_analysis (input_path)
 
+	% Inclusion and exclusion criteria
+	exclude_string = 'EXCLUDE';
+	include_string = 'WT Non-Tumor MEA GiGA1 vs. Vehicle';
+
+	% Recursively locate all '_artifact_samples.csv' files in the provided directory
 	file_list = dir ([input_path, '/**/*_artifact_samples.csv']);
 	num_files = length (file_list);
 
+	% Iterate through all files, make sure they meet inclusion and exclusion
+	% criteria, and assemble them into a list of file paths.
 	file_paths = {};
 	for i = 1:num_files
 		current_path = [file_list(i).folder, '/', file_list(i).name];
-		if ((contains (current_path, 'EXCLUDE') == 0) && (contains(current_path, 'WT Non-Tumor MEA GiGA1 vs. Vehicle') == 1))
+		if ((contains (current_path, exclude_string) == 0) && (contains(current_path, include_string) == 1))
 			file_paths = [file_paths; current_path];
 		end
 	end
 
+	% Display the list of files to be processed
 	disp (file_paths);
 
+	% Iterate through all files and do LFP analyses
 	for i = 1:length(file_paths)
 		disp(i);
 		artifact_file = file_paths{i};
