2b6ba1a7a689c12eadf47c9483e5c1365c167e61
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2022-10-05 09:38:28 -0400

    back to individual MUA plots because memory

--- RAR_workflows/RAR_workflow_plots.m
+++ RAR_workflows/RAR_workflow_plots.m
@@ -57,15 +57,12 @@ function RAR_workflow_plots (input_path)
 		GiGA1_file = strcat(folder, ', ZMG-GiGA1.ns5');
 		DMSO_file = strcat(folder, ', ZMG-DMSO.ns5');
 
-		files_to_plot = {ACSF_file};
+		RAR_plot_MUA (ACSF_file);
 		if exist (GiGA1_file,'file') == 2
-			files_to_plot = [files_to_plot; GiGA1_file];
+			RAR_plot_MUA (GiGA1_file);
 		end
 		if exist (DMSO_file,'file') == 2
-			files_to_plot = [files_to_plot; DMSO_file];
+			RAR_plot_MUA (DMSO_file);
 		end
-
-		disp(files_to_plot)
-		RAR_plot_MUA(files_to_plot{:});
 	end
 end
