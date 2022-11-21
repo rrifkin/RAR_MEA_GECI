c13064fd5727e533f6fd109aee4f2ffdd3aae715
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2022-11-21 11:53:32 -0500

    Added "inactive channels" to findpeaks and RAR_linelength

--- RAR_linelength.m
+++ RAR_linelength.m
@@ -1,29 +1,31 @@
-function RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_channels_file)
+function RAR_linelength (LFP_file, artifact_file, bad_channels_file, offslice_channels_file, inactive_channels_file, excluded_minutes)
 
     % Parameters
     sample_rate = 2000; % in Hz
-    output_suffix = '_linelength_v3';
+    output_suffix = '_linelength_v4_inactive_ch';
 
     % Import LFP data
     LFP_data = importdata (LFP_file);
 	artifact_samples = readmatrix(artifact_file);
 	bad_channels = readmatrix (bad_channels_file)
     offslice_channels = readmatrix (offslice_channels_file)
+    inactive_channels = readmatrix (inactive_channels_file)
+
+    excluded_samples = [((excluded_minutes(1) * 60 * sample_rate) + 1):(excluded_minutes(end) * 60 * sample_rate)];
 
     % Delete selected time ranges (columns) from LFP_data. 
     % artifact_samples is a N x 2 array where N is the number of
     % artifact intervals. These interval ranges are concatenated 
-    % into artifact_range, which is then deleted from LFP_data.
-    artifact_range = [];
+    % into excluded_samples, which is then deleted from LFP_data.
     for i=1:length(artifact_samples(:,1))
         current_range = artifact_samples(i,1):artifact_samples(i,2);
-        artifact_range = [artifact_range, current_range];
+        excluded_samples = [excluded_samples, current_range];
     end
-    artifact_range = unique (artifact_range);
-    LFP_data(:,artifact_range) = [];
+    excluded_samples = unique (excluded_samples);
+    LFP_data(:,excluded_samples) = [];
 
     % delete selected channels (rows) from LFP_data
-    excluded_channels = [bad_channels, offslice_channels]
+    excluded_channels = [bad_channels, offslice_channels, inactive_channels]
     excluded_channels = unique(excluded_channels)
     LFP_data(excluded_channels(:),:) = [] ;
 
