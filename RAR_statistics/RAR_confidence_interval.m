8992cc3b4e99c5fb480fe432825178831a1a5241
Robert Rifkin <47576646+rrifkin@users.noreply.github.com>
2023-04-25 12:43:35 -0400

    violinplot

--- RAR_statistics/RAR_confidence_interval.m
+++
@@ -1,7 +0,0 @@
-function CI = RAR_confidence_interval (data)
-
-	SEM = std(data,'omitnan')/sqrt(length(data));               
-	ts = tinv([0.025  0.975],length(data)-1);      
-	CI = mean(data,'omitnan') + ts*SEM;   
-
-end
